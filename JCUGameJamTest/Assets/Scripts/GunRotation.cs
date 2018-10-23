1b14f67ba39b8fea812c39fa8f1c0dd2df7a8638
SeppukuDoge <marcusheadgotscarm16@gmail.com>
2018-10-23 18:18:58 +0800

    stupid gun
    fuvck black pewople

---
+++ JCUGameJamTest/Assets/Scripts/GunRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotation : MonoBehaviour
+{
+
+    public float offset;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToViewportPoint(Input.mousePosition) - transform.position;
+        float rotationz = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationz + offset);
+    }
+}
