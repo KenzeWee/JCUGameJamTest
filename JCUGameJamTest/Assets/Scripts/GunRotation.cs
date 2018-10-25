6ab9508753b8922bfdc00d173c6673c512747b71
SeppukuDoge <marcusheadgotscarm16@gmail.com>
2018-10-25 18:19:23 +0800

    bug fixes

--- JCUGameJamTest/Assets/Scripts/GunRotation.cs
+++ JCUGameJamTest/Assets/Scripts/GunRotation.cs
@@ -4,20 +4,48 @@ using UnityEngine;
 
 public class GunRotation : MonoBehaviour
 {
-
-    public float offset;
-
+    [SerializeField]
+    private GameObject player;
+    private Camera mainCamera;
+    private float rot_z;
     // Use this for initialization
     void Start()
     {
-
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        Vector3 difference = Camera.main.ScreenToViewportPoint(Input.mousePosition) - transform.position;
-        float rotationz = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotationz + offset);
+        MouseLookat();
+        FollowMouse();
     }
+
+    private void MouseLookat()
+    {
+        Vector2 diff = GetDifference();
+        diff.Normalize();
+        rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+    }
+
+    private void FollowMouse()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 origin = player.transform.position;
+        float distanceBetween = Vector2.Distance(mousePos, origin);
+        Debug.Log(distanceBetween);
+        if (430 > distanceBetween && distanceBetween > 398)
+        {
+            this.transform.position = mainCamera.ScreenToWorldPoint(mousePos);
+        }
+    }
+
+    private Vector2 GetDifference()
+    {
+        Vector2 difference = mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        return difference;
+    }
+
+
 }
