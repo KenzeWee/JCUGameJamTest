aa6bf0b2fb34efe191357a7d18696948b465e3d0
Daraxxus <sandvichisawesome@gmail.com>
2018-10-26 10:47:56 +0800

    Fixed your gun rotation for you
    left some comments on the process of fixing it
    you can comment it out or delete it if you want to try for yourself up to you
    cheers

--- JCUGameJamTest/Assets/Scripts/GunRotation.cs
+++ JCUGameJamTest/Assets/Scripts/GunRotation.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class GunRotation : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject player;
+    [SerializeField] private GameObject player;
+
+    //Seperated gun's center to the middle (pivot point)
+    //rotate around player center
+    //move gun only towards mouse not the whole pivot
+    [SerializeField] private GameObject gun;
+
     private Camera mainCamera;
     private float rot_z;
+
+    [SerializeField] private float minDistance;
+    [SerializeField] private float maxDistance;
+    private Vector2 mousePos;
     // Use this for initialization
     void Start()
     {
@@ -17,12 +26,16 @@ public class GunRotation : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        //Get mouse pos based on world space
+        //global variable cause used throughout the script
+        mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         MouseLookat();
         FollowMouse();
     }
 
     private void MouseLookat()
     {
+        //rotate based on pivot point (this script's gameobject)
         Vector2 diff = GetDifference();
         diff.Normalize();
         rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
@@ -31,19 +44,30 @@ public class GunRotation : MonoBehaviour
 
     private void FollowMouse()
     {
-        Vector2 mousePos = Input.mousePosition;
-        Vector2 origin = player.transform.position;
-        float distanceBetween = Vector2.Distance(mousePos, origin);
-        Debug.Log(distanceBetween);
-        if (430 > distanceBetween && distanceBetween > 398)
+        //Get Distance between mouse and player
+        float distance = Vector2.Distance(mousePos, player.transform.position); 
+        //Debug.Log(distance + " " + (distance > minDistance && distance < maxDistance));
+
+        //if distance is within bounds
+        //move gun towards mouse with the max distance move clamped
+        if (distance > minDistance && distance < maxDistance)
         {
-            this.transform.position = mainCamera.ScreenToWorldPoint(mousePos);
+            gun.transform.position = Vector2.MoveTowards(gun.transform.position, mousePos, maxDistance-minDistance);
         }
+
+        //Vector2 mousePos = Input.mousePosition;
+        //Vector2 origin = player.transform.position;
+        //float distanceBetween = Vector2.Distance(mousePos, origin);
+        //Debug.Log(distanceBetween);
+        //if (430 > distanceBetween && distanceBetween > 398)
+        //{
+        //    this.transform.position = mainCamera.ScreenToWorldPoint(mousePos);
+        //}
     }
 
     private Vector2 GetDifference()
     {
-        Vector2 difference = mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Vector2 difference = mousePos - (Vector2) transform.position;
         return difference;
     }
 
