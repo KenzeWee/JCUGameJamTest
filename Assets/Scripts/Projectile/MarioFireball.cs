42e81f107df7150564764be5fefc695d2612fb8e
Daraxxus <sandvichisawesome@gmail.com>
2018-11-02 18:41:54 +0800

    Neat change

---
+++ Assets/Scripts/Projectile/MarioFireball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MarioFireball : GenericProjectile {
+
+	protected Rigidbody2D rb;
+
+	protected override void Awake()
+	{
+		base.Awake();
+		rb = GetComponent<Rigidbody2D>();	
+	}
+
+    public override void Fire()
+    {
+		rb.AddForce(transform.right * FireForce, ForceMode2D.Impulse);
+    }
+
+    protected override void collisionBehaviour()
+    {
+        
+    }
+}
