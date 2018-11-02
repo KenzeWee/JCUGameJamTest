dacea50845ac749460ef8a77c84b0379001c523c
Vauser Von Shepherd <vauservonshepherd@yahoo.com.sg>
2018-11-02 21:08:09 +0800

    GUI_Health update

---
+++ Assets/Scripts/Player/Player_GUI/PlayerInfoGUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInfoGUI : MonoBehaviour {
+    public Image GUI_PlayerHealth;
+
+    [SerializeField]
+    private PlayerVariable m_player;
+
+    private void Update()
+    {
+        UpdateHealth();
+    }
+
+    void UpdateHealth()
+    {
+        GUI_PlayerHealth.fillAmount = ((float)m_player.CurrentHP / 10f) ;
+        print(((float)m_player.CurrentHP / 10f) );
+    }
+}
