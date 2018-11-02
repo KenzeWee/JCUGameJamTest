8bde5fb6781893e154621035ed09446757bdc602
Daraxxus <sandvichisawesome@gmail.com>
2018-11-02 23:54:16 +0800

    Added level objects stuff

--- Assets/Scripts/BaseInterfacesAbstract/GenericLevel.cs
+++ Assets/Scripts/BaseInterfacesAbstract/GenericLevel.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public abstract class GenericLevel : MonoBehaviour {
 	[SerializeField] private List<Transform> respawnPoints = new List<Transform> ();
+	[SerializeField] private List<GameObject> levelObjects = new list<GameObject>();
+
+	private virtual void Awake() {
+		foreach (Transform child in transform) {
+			levelObjects.Add(child.gameObject);
+		}
+	}	
+
+	public List<GameObject> GetListOfLevelObjects () {
+		return levelObjects;
+	}
 
 	public List<Transform> GetListOfRespawnPoints () {
 		
