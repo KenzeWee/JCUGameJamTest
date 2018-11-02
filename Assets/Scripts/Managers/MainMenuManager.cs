9f93fa5284619673f69592dc25cace1957a3fc99
hydrater <hydrater@gmail.com>
2018-11-03 07:47:26 +0800

    Added main menu

---
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+    [SerializeField] private GameObject HelpScreen;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Help(bool On)
+    {
+        HelpScreen.SetActive(On);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
