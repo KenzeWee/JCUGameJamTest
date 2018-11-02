c68a6c0cc90f9fbfcfe8efcd69d7ee5fe08e2b3e
hydrater <hydrater@gmail.com>
2018-11-03 00:47:56 +0800

    Added level break

--- Assets/Scripts/BaseInterfacesAbstract/GenericLevel.cs
+++ Assets/Scripts/BaseInterfacesAbstract/GenericLevel.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public abstract class GenericLevel : MonoBehaviour {
 	[SerializeField] private List<Transform> respawnPoints = new List<Transform> ();
 	[SerializeField] private GameObject levelBreakGroup;
-	private List<GameObject> levelObjects = new List<GameObject>();
+    [SerializeField] private float lowestHeight = -15f, highestHeight = 9, fallSpeed = 10;
+    private List<GameObject> levelObjects = new List<GameObject>();
+
 
 	protected virtual void Awake() {
 		if (!levelBreakGroup) throw new ArgumentNullException ("Need to assign level break group");
@@ -14,6 +16,8 @@ public abstract class GenericLevel : MonoBehaviour {
 		foreach (Transform child in levelBreakGroup.transform) {
 			levelObjects.Add(child.gameObject);
 		}
+
+        StartCoroutine(LevelBreak());
 	}	
 
 	public List<GameObject> GetListOfLevelObjects () {
@@ -26,7 +30,30 @@ public abstract class GenericLevel : MonoBehaviour {
 		
 	}
 
-	public virtual void LevelBreak () {
-		//add level break stuff here
-	}
+	public virtual IEnumerator LevelBreak () {
+
+        //add level break stuff here
+
+        foreach (GameObject j in levelObjects)
+        {
+            if (!j.GetComponent<Rigidbody2D>())
+            {
+                levelObjects.Remove(j);
+            }
+        }
+
+        for (float i = lowestHeight; lowestHeight <= highestHeight; i+= Time.deltaTime * fallSpeed)
+        {
+            for (int j = levelObjects.Count; j > 0; --j)
+            {
+                if (levelObjects[j - 1].transform.position.y <= i)
+                {
+                    levelObjects[j - 1].GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                    levelObjects.Remove(levelObjects[j - 1]);
+                }
+            }
+
+            yield return null;
+        }
+    }
 }
