6e4c8487b485db239e2ba92ca34a4ca509585b9a
Daraxxus <sandvichisawesome@gmail.com>
2018-11-03 11:55:54 +0800

    Blackhole fix

--- Assets/Scripts/Level/BlackHole.cs
+++ Assets/Scripts/Level/BlackHole.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BlackHole : MonoBehaviour {
-	[SerializeField] public float gravityPull = .78f;
+	[SerializeField] private float gravityPull = .78f;
+	[SerializeField] private float killDistance = 0.5f;
+	[SerializeField] private bool drawDebugCircle = false;
 	private float gravityRadius = 1f;
 	void Awake () {
-		gravityRadius = GetComponent<CircleCollider2D>().radius;
+		gravityRadius = GetComponent<CircleCollider2D> ().radius;
 	}
 
-	void OnTriggerStay2D(Collider2D other) {
+	void OnTriggerStay2D (Collider2D other) {
+		if (Vector3.Distance (other.transform.position, transform.position) <= killDistance) {
+			IDamagable HP = other.GetComponent<IDamagable> ();
+			if (HP != null)
+				HP.ChangeHealth (-10000);
+		}
+
 		if (other.attachedRigidbody) {
 			float gravityIntensity = Vector3.Distance (transform.position, other.transform.position) / gravityRadius;
 			other.attachedRigidbody.AddForce ((transform.position - other.transform.position) * gravityIntensity * other.attachedRigidbody.mass * gravityPull * Time.smoothDeltaTime, ForceMode2D.Impulse);
 			//Debug.DrawRay (other.transform.position, transform.position - other.transform.position);
 		}
+	}
+
+	private void OnDrawGizmos () {
+		if (drawDebugCircle)
+			Gizmos.DrawWireSphere (transform.position, killDistance);
 	}
 }
