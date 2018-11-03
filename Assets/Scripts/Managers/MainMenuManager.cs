5a0b980bac136fd28ef184d1dc7e2505b5f3ec25
Daraxxus <sandvichisawesome@gmail.com>
2018-11-03 15:48:10 +0800

    Added main menu music and bgm music

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using TMPro;
 
 public class MainMenuManager : MonoBehaviour {
     public static MainMenuManager instance;
-
+    [SerializeField] private AudioSO mainMenuMusic;
     [SerializeField] private GameObject HelpScreen;
 
     public TextMeshProUGUI timer;
@@ -15,38 +15,35 @@ public class MainMenuManager : MonoBehaviour {
 
     private bool isCountdown = false;
 
-    private void Awake()
-    {
+    private void Awake () {
         instance = this;
+        mainMenuMusic = mainMenuMusic.Initialize (gameObject);
+        mainMenuMusic.Play();
         //timer.gameObject.SetActive(false);
     }
 
-    public void StartGame()
-    {
-        timer.gameObject.SetActive(true);
+    public void StartGame () {
+        timer.gameObject.SetActive (true);
+        mainMenuMusic.Stop();
         isCountdown = true;
     }
 
-    private void Update()
-    {
-        if (isCountdown)
-        {
+    private void Update () {
+        mainMenuMusic.Update();
+        if (isCountdown) {
             timeLeft -= Time.deltaTime;
-            timer.text = "Starting in " + timeLeft.ToString("f0");
-            if(timeLeft <= 0)
-            {
-                SceneManager.LoadScene(1);
+            timer.text = "Starting in " + timeLeft.ToString ("f0");
+            if (timeLeft <= 0) {
+                SceneManager.LoadScene (1);
             }
         }
     }
 
-    public void Help(bool On)
-    {
-        HelpScreen.SetActive(On);
+    public void Help (bool On) {
+        HelpScreen.SetActive (On);
     }
 
-    public void Exit()
-    {
-        Application.Quit();
+    public void Exit () {
+        Application.Quit ();
     }
 }
