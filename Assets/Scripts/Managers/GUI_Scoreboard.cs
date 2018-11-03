9ba3975762ea446e04f6dc8b4fd2398680ab35ac
Vauser Von Shepherd <vauservonshepherd@yahoo.com.sg>
2018-11-03 11:16:54 +0800

    Scoreboard Prepare

---
+++ Assets/Scripts/Managers/GUI_Scoreboard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_Scoreboard : MonoBehaviour {
+    public List<PlayerVariable> allPlayers = new List<PlayerVariable>();
+
+    public void ShowScoreBoard()
+    {
+        if (allPlayers.Count > 0)
+        {
+
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerVariable.cs
+++ Assets/Scripts/Player/PlayerVariable.cs
@@ -2,6 +2,8 @@
 
 [CreateAssetMenu(menuName = "PlayerHealth")]
 public class PlayerVariable : ScriptableObject {
+    public string currentName { get; set; }
+
     public int CurrentHP { get; set; }
     public int CurrentScore { get; set; }
 }
